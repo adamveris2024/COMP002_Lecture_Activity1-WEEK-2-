e1acc5468ce76b3dd4d6f16f1da5f5154ff8908d
Adam Verissimo <adamveris2024@gmail.com>
2024-10-23 19:57:38 -0700

    created script2

---
+++ script2.js
@@ -0,0 +1,14 @@
+// Lecture 2-2
+
+let myGreeting = "Hello World!";
+console.log(myGreeting);
+
+// change the string variable value
+myGreeting = "I changed the value of the string variable here.";
+console.log(myGreeting);
+
+// constant variable
+const pi = 3.14;
+// pi = 2; // throws an error because it is trying to affect a constant
+let myNumber = "5"
+console.log(Math.min(pi, myNumber)); // It chooses the lesser number, which is Pi (3.14) over the myNumber (5)
